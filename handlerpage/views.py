32e5fab7cbb4fe172bafd30aac1c1f3e6a590d1c
minji choi <mjeewh@gmail.com>
2020-08-12 00:04:34 +0900

    08.08.20

---
+++ handlerpage/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views.defaults import page_not_found
+# Create your views here.
+def handler404(request, exception):
+    return page_not_found(request, exception, template_name="404.html")
