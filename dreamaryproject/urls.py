4bb5c6ac103f91babdae648dd90b55e8d67e5fbf
minji choi <mjeewh@gmail.com>
2020-08-18 16:00:45 +0900

    18.08 시작전

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from page import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
-]
+    path('introduce/', views.introduce, name="introduce"),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+#url만들기: path('new/',views.new, name='new),
+#collectstatic은 배포전에 해도 ㅇㅋ
