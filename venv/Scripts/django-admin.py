8a41c952fc003b1265636e0055b0aff0cf69d417
minji choi <mjeewh@gmail.com>
2020-10-10 01:48:15 +0900

    [update]real...end

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\choi\desktop\django\09_10_20_elastic_beanstalk\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
